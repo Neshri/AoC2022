a53920d7055d4f2e359cc5a4a635d2d665d0abf9
Neshri <anton.lundgren94@gmail.com>
2022-12-09 17:56:39 +0100

    Day8CompleteButUgly

--- day8.py
+++ day8.py
@@ -1,50 +1,83 @@
 import numpy as np
 
-#Read
+# Read
 with open("day8input.txt") as f:
     matrix = [[int(x) for x in y.strip()] for y in f.readlines()]
 matrix = np.array(matrix)
 visibility_matrix = np.zeros((len(matrix), len(matrix[0])), dtype=np.int32)
 scenic_matrix = np.ones((len(matrix), len(matrix[0])), dtype=np.int64)
-#Left to right
+# Left to right
 for y in range(len(matrix)):
-    scene_score = 0
+    previous_heights = [0]*10
     height = -1
     for x in range(len(matrix[0])):
         if matrix[y][x] > height:
             height = matrix[y][x]
             visibility_matrix[y][x] = 1
-        scenic_matrix[y][x] *= scene_score
-            
-#Right to left
+        scenic_matrix[y][x] *= previous_heights[matrix[y][x]]
+        i = 0
+        while i <= matrix[y][x]:
+            previous_heights[i] = 1
+            i += 1
+        while i < 10:
+            previous_heights[i] += 1
+            i += 1
+# Right to left
 for y in range(len(matrix)):
+    previous_heights = [0]*10
     height = -1
     for x in reversed(range(len(matrix[0]))):
         if matrix[y][x] > height:
             height = matrix[y][x]
             visibility_matrix[y][x] = 1
-#Down to up
+        scenic_matrix[y][x] *= previous_heights[matrix[y][x]]
+        i = 0
+        while i <= matrix[y][x]:
+            previous_heights[i] = 1
+            i += 1
+        while i < 10:
+            previous_heights[i] += 1
+            i += 1
+# Down to up
 for x in range(len(matrix[0])):
+    previous_heights = [0]*10
     height = -1
     for y in range(len(matrix)):
         if matrix[y][x] > height:
             height = matrix[y][x]
             visibility_matrix[y][x] = 1
-#Up to down
+        scenic_matrix[y][x] *= previous_heights[matrix[y][x]]
+        i = 0
+        while i <= matrix[y][x]:
+            previous_heights[i] = 1
+            i += 1
+        while i < 10:
+            previous_heights[i] += 1
+            i += 1
+# Up to down
 for x in range(len(matrix[0])):
+    previous_heights = [0]*10
     height = -1
     for y in reversed(range(len(matrix))):
         if matrix[y][x] > height:
             height = matrix[y][x]
             visibility_matrix[y][x] = 1
+        scenic_matrix[y][x] *= previous_heights[matrix[y][x]]
+        i = 0
+        while i <= matrix[y][x]:
+            previous_heights[i] = 1
+            i += 1
+        while i < 10:
+            previous_heights[i] += 1
+            i += 1
 
-#Part one
+# Part one
 ans = 0
 for y in range(len(matrix)):
     for x in range(len(matrix[0])):
         ans += visibility_matrix[y][x]
 print("The first answer is: ", ans)
-#Part two
+# Part two
 ans = 0
 for y in range(len(matrix)):
     for x in range(len(matrix[0])):
