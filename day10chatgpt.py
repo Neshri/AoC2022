444f7b829c411c8d93346ee87cf3b32daeba4cf9
Neshri <anton.lundgren94@gmail.com>
2022-12-10 20:41:23 +0100

    day10PartOneComplete

---
+++ day10chatgpt.py
@@ -0,0 +1,54 @@
+# CPU simulation
+
+# Initialize the X register with the value 1
+x = 1
+
+# The addx instruction takes two cycles to complete
+def addx(v):
+    global x
+    # After two cycles, increase the value of X by v
+    x += v
+
+# The noop instruction takes one cycle to complete and has no other effect
+def noop():
+    pass
+
+# Function to check the signal strength and add it to the sum if necessary
+def check_signal_strength(cycle, signal_strength_sum):
+    if cycle >= 20 and (cycle - 20) % 40 == 0:
+        signal_strength_sum += cycle * x
+    return signal_strength_sum
+
+# Define the program by reading it from a file
+with open("day10input.txt") as f:
+    program = [line.strip() for line in f.readlines()]
+
+# Initialize the cycle counter and the sum of signal strengths
+cycle = 1
+signal_strength_sum = 0
+
+# Execute each instruction in the program
+for instruction in program:
+    # Increase the cycle counter by one
+    cycle += 1
+    # The noop instruction takes one cycle to complete
+    if instruction == "noop":
+        # Do nothing
+        pass
+    # The addx instruction takes two cycles to complete
+    elif "addx" in instruction:
+        # Add the current signal strength to the sum if the cycle counter is part of the series 20, 60, 100, 140, etc.
+        signal_strength_sum = check_signal_strength(cycle, signal_strength_sum)
+        
+        # Get the value to add to X from the instruction
+        value = int(instruction.split()[1])
+        # Execute the addx instruction
+        addx(value)
+        # Increase the cycle counter by one
+        cycle += 1
+
+    # Add the current signal strength to the sum if the cycle counter is part of the series 20, 60, 100, 140, etc.
+    signal_strength_sum = check_signal_strength(cycle, signal_strength_sum)
+
+# Output the sum of signal strengths
+print(f"Sum of signal strengths: {signal_strength_sum}")
