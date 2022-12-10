bf62c9925f59bdff2c9de24d3a1ad4fad884a29f
Neshri <anton.lundgren94@gmail.com>
2022-12-10 21:49:43 +0100

    Part2Complete

--- day10chatgpt.py
+++ day10chatgpt.py
@@ -19,6 +19,12 @@ def check_signal_strength(cycle, signal_strength_sum):
         signal_strength_sum += cycle * x
     return signal_strength_sum
 
+def draw(cycle, x, screen):
+    cycle -= 1
+    if x-1 <= cycle % 40 <= x+1:
+        screen[cycle//len(screen[0])][cycle%40] = '#'
+    
+
 # Define the program by reading it from a file
 with open("day10input.txt") as f:
     program = [line.strip() for line in f.readlines()]
@@ -26,7 +32,10 @@ with open("day10input.txt") as f:
 # Initialize the cycle counter and the sum of signal strengths
 cycle = 1
 signal_strength_sum = 0
-
+screen = []
+for i in range(6):
+    screen.append([' '] * 40)
+draw(cycle, x, screen)
 # Execute each instruction in the program
 for instruction in program:
     # Increase the cycle counter by one
@@ -39,7 +48,7 @@ for instruction in program:
     elif "addx" in instruction:
         # Add the current signal strength to the sum if the cycle counter is part of the series 20, 60, 100, 140, etc.
         signal_strength_sum = check_signal_strength(cycle, signal_strength_sum)
-        
+        draw(cycle, x, screen)
         # Get the value to add to X from the instruction
         value = int(instruction.split()[1])
         # Execute the addx instruction
@@ -49,6 +58,10 @@ for instruction in program:
 
     # Add the current signal strength to the sum if the cycle counter is part of the series 20, 60, 100, 140, etc.
     signal_strength_sum = check_signal_strength(cycle, signal_strength_sum)
+    draw(cycle, x, screen)
 
 # Output the sum of signal strengths
 print(f"Sum of signal strengths: {signal_strength_sum}")
+
+for line in screen:
+    print("".join(line))
