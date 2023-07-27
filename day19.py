01340f6056b28f554e9dececc89684b57a4ddb04
Anton <neshri@github.com>
2023-07-28 01:23:37 +0200

    replaced some dictionaries with integers and bitmasks

--- day19.py
+++ day19.py
@@ -2,7 +2,7 @@ import re
 import time
 t = time.perf_counter()
 
-BITMASKS = {"geode": 0xFFFF << 48, "obsidian": 0xFFFF << 32, "clay": 0xFFFF << 16, "ore": 0xFFFF}
+BITMASKS = {"geode": (0xFFFF << 48, 48), "obsidian": (0xFFFF << 32, 32), "clay": (0xFFFF << 16, 16), "ore": (0xFFFF, 0)}
 
 class Blueprint:
     def __init__(self, bp) -> None:
@@ -24,36 +24,30 @@ class Blueprint:
         string = re.findall("Each geode robot [ A-Za-z0-9]+", bp)[0]
         self.geode_robot = get_cost(string)
 
-def dict_hash(d):
-    hash_value = 0
-    for key in d.keys():
-        hash_value += hash((hash(d[key]), hash(key)))
-    return hash_value
-
+def get_bits(i, key):
+    return (i & BITMASKS[key][0]) >> BITMASKS[key][1]
         
 def max_g_crack(time_limit, bp):
-    
     hash_check = dict()
     costs = {"geode": bp.geode_robot, "obsidian": bp.obsidian_robot, "clay": bp.clay_robot, "ore": bp.ore_robot}
+    # Optimiziation pruning
+    max_bot_need = {"geode": float("inf"), "obsidian": 0, "clay": 0, "ore": 0}
+    for k in costs.keys():
+        for k2 in costs[k].keys():
+            if costs[k][k2] > max_bot_need[k2]:
+                max_bot_need[k2] = costs[k][k2]
     def recursive_crack(time_limit, resources, robots, best_crack):
         # Bottom reached
         if time_limit <= 0:
+            return best_crack    
+        # return if there's more bots than needed
+        if costs["geode"]["obsidian"] < get_bits(robots, "obsidian") or costs["obsidian"]["clay"] < get_bits(robots, "clay"):
             return best_crack
-        
-        # Optimiziation pruning
-        max_bot_need = {"geode": float("inf"), "obsidian": 0, "clay": 0, "ore": 0}
-        for k in costs.keys():
-            for k2 in costs[k].keys():
-                if costs[k][k2] > max_bot_need[k2]:
-                    max_bot_need[k2] = costs[k][k2]
-        
-        if costs["geode"]["obsidian"] < robots["obsidian"] or costs["obsidian"]["clay"] < robots["clay"]:
-            return best_crack
-        
         
         for i in range(1, time_limit + 1):
+            # return if there's no chance of beating record
             time_left = time_limit - i
-            if resources["geode"] + robots["geode"] * (time_left + 1) + (time_left * time_left + time_left) / 2 <= best_crack:
+            if get_bits(resources, "geode") + get_bits(robots, "geode") * (time_left + 1) + (time_left * time_left + time_left) / 2 <= best_crack:
                 return best_crack    
             
             building = list()     
@@ -62,49 +56,46 @@ def max_g_crack(time_limit, bp):
                 cost = costs[k]
                 can_afford = True
                 for x in cost.keys():
-                    if cost[x] > resources[x]:
+                    if cost[x] > get_bits(resources, x):
                         can_afford = False
                         break
-                if can_afford and robots[k] < max_bot_need[k]:
+                if can_afford and get_bits(robots, k) < max_bot_need[k]:
                     building.append(k)
                     
             # Drilling
-            for x in robots.keys():
-                resources[x] += robots[x]
+            resources += robots
             
             # Finish bot building   
             for robot in building:
-                new_resources = dict(resources)
-                new_robots = dict(robots)
+                new_resources = resources
+                new_robots = robots
                 cost = costs[robot]
                 for x in cost.keys():
-                    new_resources[x] -= cost[x]
-                new_robots[robot] += 1
-                hash_value = (dict_hash(new_resources), dict_hash(new_robots))
-                
-                
+                    new_resources -= (cost[x] << BITMASKS[x][1])
+                new_robots += (1 << BITMASKS[robot][1])
+                hash_value = (new_resources, new_robots)
                 
                 if (hash_value not in hash_check.keys() or 
                     hash_check[hash_value][1] < time_limit - i):
                     
-                    tmp = recursive_crack(time_limit - i, dict(new_resources), new_robots, best_crack)
+                    tmp = recursive_crack(time_limit - i, new_resources, new_robots, best_crack)
                     if hash_value not in hash_check.keys() or hash_check[hash_value][0] < tmp:
                         hash_check[hash_value] = (tmp, time_limit - i)
-                        # print(hash_check[hash_value])
                 else:
                     tmp = hash_check[hash_value][0]
                 if tmp > best_crack:
                     best_crack = tmp
                 
         
-        if resources["geode"] > best_crack:
-            best_crack = resources["geode"]
+        if get_bits(resources, "geode") > best_crack:
+            best_crack = get_bits(resources, "geode")
         return best_crack
     
     resources = {"ore": 0, "clay": 0, "obsidian": 0, "geode": 0}
     robots = {"ore": 1, "clay": 0, "obsidian": 0, "geode": 0}
-    # resources = 0
-    # robots = 1
+    resources = 0
+    robots = 0
+    robots += 1 << BITMASKS["ore"][1]
     
     ans = recursive_crack(time_limit, resources, robots, 0)
     return ans
